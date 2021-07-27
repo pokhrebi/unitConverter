59ddb962574328f5c7933a343c89d3c9ccd71de9
pokhrebi <pokhrebi@dev.azure.com>
2021-07-27 00:15:08 -0400

    Comit

---
+++ src/unitconverter/converterWindow.java
@@ -0,0 +1,20 @@
+
+package unitconverter;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author bhakti
+ */
+public class converterWindow 
+{
+    private JTextField  farenheitTF;
+    private JTextField celsiusTF;
+    private JButton convertBtn;
+    private JLabel farenheitLabel;
+    private JLabel celsiusLabel;
+    
+}
